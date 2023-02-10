6eddb7b558cc5d43b8d9615109917b5344ee4f03
xiaxudong <xiaxudong@xiaomi.com>
2023-02-10 17:13:55 +0800

    service

--- ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
+++ ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.servicetest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Service;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private MyService.DownloadBinder downloadBinder;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            downloadBinder = (MyService.DownloadBinder) service;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +37,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button startService = findViewById(R.id.start_service);
         Button stopService = findViewById(R.id.stop_service);
 
+        Button bindService = findViewById(R.id.bind_service);
+        Button unBindService = findViewById(R.id.unbind_service);
+        bindService.setOnClickListener(this);
+        unBindService.setOnClickListener(this);
     }
 
     @Override
@@ -29,6 +53,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.stop_service:
                 Intent stopIntent = new Intent(this, MyService.class);
                 stopService(stopIntent);
+                break;
+            case R.id.bind_service:
+                Intent bindIntent = new Intent(this, MyService.class);
+                bindService(bindIntent, connection, BIND_AUTO_CREATE); // 绑定服务
+                break;
+            case R.id.unbind_service:
+                unbindService(connection); // 解绑服务
                 break;
             default:
                 break;
--- ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
+++ ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
@@ -1,22 +1,44 @@
 package com.example.servicetest;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
 
 public class MyService extends Service {
+
+    private DownloadBinder mBinder = new DownloadBinder();
+
+    class DownloadBinder extends Binder {
+        public void startDownload() {
+            Log.d("MyService", "startDownload executed");
+        }
+
+        public int getProgress() {
+            Log.d("MyService", "getProgress executed");
+            return 0;
+        }
+    }
+
     public MyService() {
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d("MyService", "onCreate executed");
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
     }
 
     @Override
