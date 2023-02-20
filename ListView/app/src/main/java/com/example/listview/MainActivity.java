eaf4d1a00dadd8b2e1fca9f586784a6846ecdc7a
xiaxudong <xiaxudong@xiaomi.com>
2023-02-20 18:36:18 +0800

    增加ListView点击事件

--- ListView/app/src/main/java/com/example/listview/MainActivity.java
+++ ListView/app/src/main/java/com/example/listview/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
         FruitAdapter adapter = new FruitAdapter(this, R.layout.fruit_item, fruitList);
         ListView listView = findViewById(R.id.list_view);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Fruit fruit = fruitList.get(position);
+                Toast.makeText(MainActivity.this, fruit.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void initFruits() {
