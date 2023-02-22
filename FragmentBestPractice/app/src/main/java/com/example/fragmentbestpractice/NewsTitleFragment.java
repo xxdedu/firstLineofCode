709d1f41ec820ac315098370cb1848c74ad911e9
xiaxudong <1429631654@qq.com>
2023-02-23 00:58:03 +0800

    test

---
+++ FragmentBestPractice/app/src/main/java/com/example/fragmentbestpractice/NewsTitleFragment.java
@@ -0,0 +1,61 @@
+package com.example.fragmentbestpractice;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NewsTitleFragment extends Fragment {
+
+    private boolean isTwoPane;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.news_title_frag, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (getActivity().findViewById(R.id.news_content_layout) != null) {
+            // 可以找到news_content_layout布局时，为双页模式
+            isTwoPane = true;
+        } else {
+            isTwoPane = false;
+        }
+    }
+
+    class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+
+            }
+        }
+
+        @NonNull
+        @Override
+        public NewsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull NewsAdapter.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    }
+}
