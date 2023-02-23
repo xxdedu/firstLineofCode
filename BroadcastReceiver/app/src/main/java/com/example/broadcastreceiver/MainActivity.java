259e0e7382b0dda3be1d065505e48cd2d8419f73
xiaxudong <xiaxudong@xiaomi.com>
2023-02-23 14:47:28 +0800

    接收网络变化发出的广播

---
+++ BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.broadcastreceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NetworkChangeReceiver networkChangeReceiver;
+
+    private IntentFilter intentFilter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 如果组件的 IntentFilter 与 Intent 中的 IntentFilter 正好匹配，系统就会启动该组件，
+        // 并把 Intent 传递给它。如果有多个组件同时匹配到了，系统则会弹出一个选择框，让用户选择使
+        // 用哪个应用去处理这个 Intent，比如有时候点击一个网页链接，会弹出多个应用，让用户选择用
+        // 哪个浏览器去打开该链接，就是这种情况。
+        intentFilter = new IntentFilter();
+        // 当网络状态发生变化时，系统发出的正是一条值为android.net.conn.CONNECTIVITY_CHANGE的广播
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        networkChangeReceiver = new NetworkChangeReceiver();
+        registerReceiver(networkChangeReceiver, intentFilter);
+    }
+
+    /**
+     * 动态注册的广播接收器一定要取消注册
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(networkChangeReceiver);
+    }
+
+    class NetworkChangeReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "network changes", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
