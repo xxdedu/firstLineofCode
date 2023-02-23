710918a72a6e6acab19546a62054b7c5270961df
xiaxudong <xiaxudong@xiaomi.com>
2023-02-23 14:53:52 +0800

    优化广播接收器的逻辑

--- BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/MainActivity.java
+++ BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -43,7 +45,14 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onReceive(Context context, Intent intent) {
-            Toast.makeText(context, "network changes", Toast.LENGTH_SHORT).show();
+//            Toast.makeText(context, "network changes", Toast.LENGTH_SHORT).show();
+            ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+            if (activeNetworkInfo != null && activeNetworkInfo.isAvailable()) {
+                Toast.makeText(context, "network is available", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, "network is unavailable", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
