7836a0cfb895d99aeda08d773bcf7ea516eb268e
xiaxudong <xiaxudong@xiaomi.com>
2023-02-23 10:47:40 +0800

    test

--- FragmentBestPractice/app/src/main/java/com/example/fragmentbestpractice/NewsContentActivity.java
+++ FragmentBestPractice/app/src/main/java/com/example/fragmentbestpractice/NewsContentActivity.java
@@ -1,14 +1,33 @@
 package com.example.fragmentbestpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class NewsContentActivity extends AppCompatActivity {
+
+    public static void actionStart(Context context, String newsTitle, String newsContent) {
+        Intent intent = new Intent(context, NewsContentActivity.class);
+        intent.putExtra("news_title", newsTitle);
+        intent.putExtra("news_content", newsContent);
+        context.startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news_content);
+        // 获取传入的新闻标题
+        String newsTitle = getIntent().getStringExtra("news_title");
+        // 获取传入的新闻内容
+        String newsContent = getIntent().getStringExtra("news_content");
+        // 得到NewsContentFragment的实例
+        NewsContentFragment newsContentFragment = (NewsContentFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.news_content_fragment);
+        // 刷新NewsContentFragment界面
+        newsContentFragment.refresh(newsTitle, newsContent);
     }
 }
---
+++ FragmentBestPractice/app/src/main/java/com/example/fragmentbestpractice/NewsTitleFragment.java
@@ -0,0 +1,10 @@
+package com.example.fragmentbestpractice;
+
+import androidx.fragment.app.Fragment;
+
+public class NewsTitleFragment extends Fragment {
+
+    private boolean isTwoPane;
+
+
+}
