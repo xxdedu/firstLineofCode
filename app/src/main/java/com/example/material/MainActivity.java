2ad239209793e5089d00be5607a8baa8519fdf02
xiaxudong <xiaxudong@xiaomi.com>
2023-03-01 22:19:54 +0800

    material

---
+++ app/src/main/java/com/example/material/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.material;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
