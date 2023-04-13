05e6f441279bc8d7c425937868704afc4d77c7af
xiaxudong <xiaxudong@xiaomi.com>
2023-04-13 10:35:40 +0800

    a

--- DatabaseTest/app/src/main/java/com/example/databasetest/MainActivity.java
+++ DatabaseTest/app/src/main/java/com/example/databasetest/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.databasetest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MyDatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 1);
+        Button createDatabase = findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 第一次调用时调用onCreate方法，数据库存在时调用onOpen方法
+                dbHelper.getWritableDatabase();
+            }
+        });
     }
 }
